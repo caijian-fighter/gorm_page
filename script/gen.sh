ab35160b5899d17e875f41401efdea3b9b9cd971
ccjian <13580782518@qq.com>
2023-04-22 20:22:12 +0800

    gorm_page封装

---
+++ script/gen.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# 生成sql代码， 进入 "服务/dal"
+# gentool -c "./gen.tool"
+
+# 生成sql代码， 进入 "服务/dal/query"
+# gentool -dsn "root:123456@tcp(192.168.47.166:3306)/shared_usercenter?charset=utf8mb4&parseTime=True&loc=Local" -modelPkgName="./model"
